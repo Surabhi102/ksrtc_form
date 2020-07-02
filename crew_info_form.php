148017741c701d8c0cacce0459768edddcf1f3a3
surabhi <project.surabhi@gmail.com>
2020-07-02 09:18:33 +0530

    ksrtc_form

---
+++ crew_info_form.php
@@ -0,0 +1,108 @@
+<?php
+include ("connect.php");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    $name = $_POST["Name"];
+    $PF_number = $_POST["PF_number"];
+    $Badge_number = $_POST["Badge_number"];
+    $Bus_number = $_POST["Bus_number"];
+    $Route_number = $_POST["Route_number"];
+    $status= 1;
+    
+    $sqll="INSERT INTO `crew_info`(`Name`, `PF_no`, `Badge_no`, `Bus_no`, `Route_no`, `stautus`) 
+    VALUES ('$name','$PF_number','$Badge_number','$Bus_number','$Route_number','$status')";
+    $resultt = mysqli_query($link,$sqll); 
+    if($resultt == true){
+        echo '<script>alert("Logeed in succesfully!!")</script>';  
+        echo '<script>window.location="crew_succes.php"</script>'; 
+    }
+    else{
+        echo "error!!!";
+    }
+    // $sqll =" SELECT `Name`, `PF number`, `Badge number`, `Bus number`, `Route number`, `Date/ Time` FROM `crewform` WHERE 1)";
+    // $db = mysql_select_db("KSRTC Form", $configure-form);
+    // $query = mysql_query("select * from crewform");
+    // mysql_select_db('test_db'); 
+    // $resultt = mysqli_query($link,$sqll); 
+    // if($resultt == true){
+    //     echo "submittedddd";
+    // }
+    // else{
+    //     echo "errorrrr!!!";
+    // }
+    // while($row = mysql_fetch_assoc($resultt)) {
+    // echo "EMP ID :{$row['Name']}  <br> ".
+    // "EMP NAME : {$row['PF number']} <br> ".
+    // "EMP SALARY : {$row['Badge number']} <br> ".
+    // "--------------------------------<br>";
+    // }
+
+ }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>newform</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+<form action="" method = "POST">
+<div class="background">
+    <div class="container">
+      <div class="screen">
+        <div class="screen-header">
+        <div class= "formm">
+            <div class="screen-header-button close"></div>
+            <div class="screen-header-button maximize"></div>
+            <div class="screen-header-button minimize"></div>
+          </div>
+          <div class="screen-header-">
+          </div>
+        </div>
+        <div class="screen-body">
+          <div class="screen-body-item left">
+            <div class="app-title">
+              <span   >KSRTC Crew Info </span><br>
+              
+            </div>
+            <div class="app-contact"></div>
+          </div>
+          <div class="screen-body-item">
+            <div class="app-form">
+              <div class="app-form-group">
+                <input class="app-form-control" name = "Name" placeholder="NAME" required >
+              </div>
+              <div class="app-form-group">
+                <input class="app-form-control" name = "PF_number" placeholder="PF number" required >
+              </div>
+              <div class="app-form-group">
+                <input class="app-form-control" name = "Badge_number" placeholder="Badge number" required >
+              </div>
+              <div class="app-form-group">
+                <input class="app-form-control" name = "Bus_number" placeholder="Bus number" required >
+              </div>
+              <div class="app-form-group">
+                <input class="app-form-control" name = "Route_number" placeholder="Route number" required >
+              </div>
+              
+              <div class="app-form-group message">
+                <button class="app-form-button">CANCEL</button>
+                <button class="app-form-button">SEND</button>
+              </div>
+            </div>
+        </div>
+      </div>
+      </form>
+      
+    </div>
+  </div>
+</div>
+  </body>
+</html>
---
+++ passenger_info.php
@@ -0,0 +1,142 @@
+<?php
+
+include ("connect.php");
+$bus_no='12';
+$sql1 = "SELECT `Route_no` FROM `crew_info` WHERE Bus_no='$bus_no'";
+$result1 = mysqli_query($link,$sql1);
+$row = mysqli_fetch_assoc($result1);
+$res = $row["Route_no"];
+
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["Name"];
+    $phone_Number = $_POST["Phone_Number"];
+    $sql = "INSERT INTO `passenger_info`( `name`, `phone`, `bus_no`, `route_no`)
+     VALUES ('$name','$phone_Number','$bus_no','$res')";
+
+    $result = mysqli_query($link,$sql);
+
+    if($result == true){
+      echo '<script>alert("Thank you!!")</script>';  
+      echo '<script>window.location="passenger_info.php"</script>'; 
+    }
+
+    else{
+        echo "error!!!";
+    }
+
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>newform</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+<?php
+if(mysqli_num_rows($result1)==0){
+    ?>
+        <h1 style="text-align:ceter;color:white">Sorry cannot acess the form!!!! </h1>
+<?php        
+}else{
+    
+?>
+
+
+<form action="" method = "POST">
+
+<div class="background">
+
+    <div class="container">
+
+      <div class="screen">
+
+        <div class="screen-header">
+
+        <div class= "formm">
+
+            <div class="screen-header-button close"></div>
+
+            <div class="screen-header-button maximize"></div>
+
+            <div class="screen-header-button minimize"></div>
+
+          </div>
+
+          <div class="screen-header-">
+
+          </div>
+
+        </div>
+
+        <div class="screen-body">
+
+          <div class="screen-body-item left">
+
+            <div class="app-title">
+
+              <span   >KSRTC Passenger Info </span><br>
+            </div>
+
+            <div class="app-contact"></div>
+
+          </div>
+
+          <div class="screen-body-item">
+
+            <div class="app-form">
+
+              <div class="app-form-group">
+
+                <input class="app-form-control" name = "Name"  placeholder="NAME" required >
+
+              </div>
+
+              <div class="app-form-group">
+
+                <input class="app-form-control" name = "Phone_Number" pattern="[0-9]{10}" placeholder="CONTACT NO" required >
+
+              </div>
+
+        
+
+              <div class="app-form-group message">
+
+                <button class="app-form-button">CANCEL</button>
+
+                <button class="app-form-button">SEND</button>
+
+              </div>
+
+            </div>
+
+        </div>
+
+      </div>
+
+      </form>
+      <?php        
+}
+    
+?>
+
+  </body>
+
+</html>
